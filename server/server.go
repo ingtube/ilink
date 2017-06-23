70e196d954e8b13920092567f12d31916f47fc6a
yunair <airzhaoyn@gmail.com>
2017-06-23 17:42:41 +0800

    move port back

--- server/server.go
+++ server/server.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-	host = flag.String("host", ":9999", "HTTPS 服务器 <ip>:<port>")
+	host = flag.String("host", ":443", "HTTPS 服务器 <ip>:<port>")
 	//domainCertificateFile = flag.String("domain_certificate_file", "", "域名 HTTPS 证书文件")
 	//domainKeyFile         = flag.String("domain_key_file", "", "域名 HTTPS key 文件")
 	staticFile = flag.String("static_file", "", "")
